2c6719b35fd5fd9410af244cdac60328e453d8f7
Temirkhan <temirkhan.nasukhov@gmail.com>
2019-09-22 20:45:31 +0300

    validator included

---
+++ src/View/ConstraintViolationsView.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\View;
+
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Temirkhan\View\ViewInterface;
+
+/**
+ * Validation errors view
+ */
+class ConstraintViolationsView implements ViewInterface
+{
+    /**
+     * Creates view
+     *
+     * @param mixed $context
+     *
+     * @return array
+     */
+    public function getView($context): array
+    {
+        if (!$context instanceof ConstraintViolationListInterface) {
+            return null;
+        }
+
+        $view = [];
+        /**
+         * @var ConstraintViolationInterface $violation
+         */
+        foreach ($context as $violation) {
+            $view[$violation->getPropertyPath()] = $violation->getMessage();
+        }
+
+        return $view;
+    }
+}
