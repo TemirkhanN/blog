418bbf844e4859ecc1eff4dccac05b61ef335d28
Temirkhan <temirkhan.nasukhov@gmail.com>
2020-09-21 21:41:30 +0300

    Fixed preview cut

--- src/Service/TextFormatter.php
+++ src/Service/TextFormatter.php
@@ -8,6 +8,22 @@ class TextFormatter
 {
     public static function cutFirstSentences(string $fromString, int $amountOfSentences): string
     {
-        return implode('.', explode('.', $fromString, $amountOfSentences));
+        if ($amountOfSentences < 1) {
+            throw new \UnexpectedValueException('Amount of the sentences can not be lesser than 1.');
+        }
+
+        $allSentences = explode('.', $fromString);
+        $amountOfAllSentences = count($allSentences);
+
+        if ($amountOfSentences >= $amountOfAllSentences) {
+            return $fromString;
+        }
+
+        $sentences = [];
+        for ($i = 0; $i < $amountOfSentences && $i < $amountOfAllSentences; $i++) {
+            $sentences[] = $allSentences[$i];
+        }
+
+        return implode('.', $sentences) . '...';
     }
 }
