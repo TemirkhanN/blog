a3165362e80bb261b4af4e4e33da28efe867d4ea
Temirkhan <temirkhan.nasukhov@gmail.com>
2021-06-25 19:35:19 +0300

    Created disclaimer

--- frontend/src/post/Post.tsx
+++ frontend/src/post/Post.tsx
@@ -4,6 +4,7 @@ import TagList from "./TagList";
 import {Helmet} from "react-helmet-async";
 import {Alert, Spinner} from "react-bootstrap";
 import {useEffect, useState} from "react";
+import Disclaimer from "../Disclaimer";
 
 type PostModel = {
     slug: string,
@@ -80,23 +81,25 @@ function Post(props: { match: { params: { slug: string } } }) {
     );
 
     return (
-        <div className="post">
-            <Helmet>
-                <title>{post.title}</title>
-            </Helmet>
-            <h1>{post.title}</h1>
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
-                     className="bi bi-calendar" viewBox="0 0 16 16">
-                    <path
-                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-                </svg>
-                <span className="pub-date">{publishedAt}</span>
+        <>
+            <div className="post">
+                <Helmet>
+                    <title>{post.title}</title>
+                </Helmet>
+                <h1>{post.title}</h1>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor"
+                         className="bi bi-calendar" viewBox="0 0 16 16">
+                        <path
+                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+                    </svg>
+                    <span className="pub-date">{publishedAt}</span>
+                </div>
+                <TagList tags={post.tags}/>
+                <div className="content" dangerouslySetInnerHTML={{__html: content}}/>
             </div>
-
-            <TagList tags={post.tags}/>
-            <div className="content" dangerouslySetInnerHTML={{__html: content}}/>
-        </div>
+            <Disclaimer/>
+        </>
     );
 }
 
