b68098df47427574529aaac29ebe03ae08c5bb93
Temirkhan <temirkhan.nasukhov@gmail.com>
2021-06-28 00:07:35 +0300

    Fixed pagination for general post list

--- src/Controller/Post/ListController.php
+++ src/Controller/Post/ListController.php
@@ -34,10 +34,11 @@ class ListController
         $tag = $request->query->getAlnum('tag');
         if ($tag !== '') {
             $posts = $this->postListService->getPostsByTag($tag, $offset, self::POSTS_PER_PAGE);
+            $ofTotalPosts = $this->postListService->countPosts($tag);
         } else {
             $posts = $this->postListService->getPosts($offset, self::POSTS_PER_PAGE);
+            $ofTotalPosts = $this->postListService->countPosts();
         }
-        $ofTotalPosts = $this->postListService->countPosts($tag);
 
         $context = new CollectionChunk(self::POSTS_PER_PAGE, $offset, $ofTotalPosts, $posts);
 
