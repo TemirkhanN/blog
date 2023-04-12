aa951cd741937a3399c3ce0a8fb31447892cb338
Temirkhan <temirkhan@nasukhov.me>
2023-04-12 23:37:30 +0200

    Code review changes

--- frontend/API/PostsCollection.php
+++ frontend/API/PostsCollection.php
@@ -4,14 +4,27 @@ declare(strict_types=1);
 
 namespace Frontend\API;
 
+use Frontend\API\Model\Post;
+use IteratorAggregate;
 use Traversable;
 
-class PostsCollection implements \IteratorAggregate
+/**
+ * @template-covariant Post
+ * @extends            IteratorAggregate<Post>
+ */
+class PostsCollection implements IteratorAggregate
 {
+    /**
+     * @param Post[]   $posts
+     * @param Metadata $metadata
+     */
     public function __construct(public readonly array $posts, public readonly Metadata $metadata)
     {
     }
 
+    /**
+     * @return Traversable<Post>
+     */
     public function getIterator(): Traversable
     {
         yield from $this->posts;
