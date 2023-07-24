c3e09aea93082d626e830f5ea18984aabfeaa67f
Temirkhan <temirkhan@nasukhov.me>
2023-07-24 13:44:33 +0200

    Fixed cert renewal

--- .docker/certbot-nginx.sh
+++ .docker/certbot-nginx.sh
@@ -6,4 +6,4 @@ apt update -yq && apt install -y python3 python3-venv libaugeas0 \
 && /opt/certbot/bin/pip install certbot certbot-nginx \
 && ln -s /opt/certbot/bin/certbot /usr/bin/certbot \
 && certbot --nginx \
-&& echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null
+&& echo "0 0 1 * * root certbot renew -q" | tee -a /etc/crontab > /dev/null
