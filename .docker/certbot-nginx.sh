478a9fd9178701642e3ad18d6a54ae63de7b245a
Temirkhan <temirkhan@nasukhov.me>
2023-12-24 22:53:10 +0100

    fixed cert renewal

--- .docker/certbot-nginx.sh
+++ .docker/certbot-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt update -yq && apt install -y python3 python3-venv libaugeas0 \
+apt update -yq && apt install -y python3 python3-venv libaugeas0 cron \
 && python3 -m venv /opt/certbot/ \
 && /opt/certbot/bin/pip install --upgrade pip \
 && /opt/certbot/bin/pip install certbot certbot-nginx \
