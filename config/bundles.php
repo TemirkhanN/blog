7e46c90a1cb0778331b93678ab8a6ca4f7d436bb
Temirkhan <temirkhan@nasukhov.me>
2024-03-03 08:59:07 +0100

    Replaced dto-bundle with symfony serializer

--- config/bundles.php
+++ config/bundles.php
@@ -5,8 +5,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Ser\DtoRequestBundle\SerDtoRequestBundle::class => ['all' => true],
 ];
