aff208f590e423abac717abfa63f58e96b3056fd
Temirkhan <temirkhan@nasukhov.me>
2024-06-10 00:28:47 +0200

    Fixing conflict with nginx

--- .docker/client/docker-entrypoint.d/100-cert.sh
+++ .docker/client/docker-entrypoint.d/100-cert.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+# There has to be more adequate way to resolve this. Currently installing certificate before running nginx issues conflict
+# on binding port.
+run_with_delay() {
+    local delay=$1
+    shift
+    local command="$@"
+
+    # Delay execution
+    sleep "$delay" && $command &
+}
+
 CHAIN_FILE=/etc/letsencrypt/live/$BLOG_DOMAIN_NAME/fullchain.pem
 
 if [ ! -f $CHAIN_FILE ]; then
-  certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --agree-tos -n
+  run_with_delay 5 certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --agree-tos &
 else
-  certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --reinstall -n
+  run_with_delay 5 certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --reinstall &
 fi
 
 cron
