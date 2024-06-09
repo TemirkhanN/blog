0997c0905dba755c0bc57e21d923e59158ebe1e0
Temirkhan <temirkhan@nasukhov.me>
2024-06-09 22:53:52 +0200

    Fixed missing interaction with certbot

--- .docker/client/docker-entrypoint.d/100-cert.sh
+++ .docker/client/docker-entrypoint.d/100-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /etc/letsencrypt/live/temirkhan.nasukhov.me/fullchain.pem ]; then
-  certbot --nginx -d $BLOG_DOMAIN_NAME --no-redirect
+  certbot --nginx -d $BLOG_DOMAIN_NAME --no-redirect --non-interactive -m $SSLCERT_OWNER_EMAIL
 fi
 
 cron
