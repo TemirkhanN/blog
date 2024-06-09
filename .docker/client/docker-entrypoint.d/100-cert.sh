a27e1952b124900eab19b54f422d55af3aae6ea2
Temirkhan <temirkhan@nasukhov.me>
2024-06-09 22:56:57 +0200

    ACME agreement autoaccept

--- .docker/client/docker-entrypoint.d/100-cert.sh
+++ .docker/client/docker-entrypoint.d/100-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /etc/letsencrypt/live/temirkhan.nasukhov.me/fullchain.pem ]; then
-  certbot --nginx -d $BLOG_DOMAIN_NAME --no-redirect --non-interactive -m $SSLCERT_OWNER_EMAIL
+  certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --agree-tos --no-redirect --non-interactive
 fi
 
 cron
