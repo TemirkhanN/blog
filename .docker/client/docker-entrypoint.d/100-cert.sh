dbb194081c5a2e844cabd4701c082c275bba7a55
Temirkhan <temirkhan@nasukhov.me>
2024-06-10 00:03:38 +0200

    Trying to fix certificate renewal

--- .docker/client/docker-entrypoint.d/100-cert.sh
+++ .docker/client/docker-entrypoint.d/100-cert.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f /etc/letsencrypt/live/temirkhan.nasukhov.me/fullchain.pem ]; then
   certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --agree-tos --no-redirect --non-interactive
+else
+  certbot --nginx -d $BLOG_DOMAIN_NAME -m $SSLCERT_OWNER_EMAIL --reinstall
 fi
 
 cron
