96f9db313b2d22628eb37fce9da020a54331d7b8
Temirkhan <temirkhan@nasukhov.me>
2025-02-07 11:23:51 +0100

    Tags are no longer in sitemap

--- src/Service/SitemapGenerator.php
+++ src/Service/SitemapGenerator.php
@@ -33,25 +33,14 @@ readonly class SitemapGenerator
             '<?xml version="1.0"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"/>'
         );
 
-        $tags = [];
         foreach ($this->postRepository->getPosts($filter) as $post) {
             $lastMod = $post->updatedAt() ?? $post->createdAt();
-            foreach ($post->tags() as $tag) {
-                $tags[$tag] = true;
-            }
 
             $url = $doc->addChild('url');
             $url->addChild('loc', $this->uriResolver->resolvePostUri($post->id(), $post->slug()));
             $url->addChild('lastmod', $lastMod->format(DATE_ATOM));
             $url->addChild('changefreq', self::FREQ_YEARLY);
             $url->addChild('priority', self::PRIORITY_TOP);
-        }
-
-        foreach (array_keys($tags) as $tag) {
-            $url = $doc->addChild('url');
-            $url->addChild('loc', $this->uriResolver->resolveTaggedPostsUri($tag));
-            $url->addChild('changefreq', self::FREQ_MONTHLY);
-            $url->addChild('priority', self::PRIORITY_NORMAL);
         }
 
         return $doc->asXML() ? : '';
